b23b1da81af722d0f2750ea6e4fa872390bac2e8
Csaba Gaspar <cgaspar@accretivetg.com>
2024-12-19 07:59:44 +0100

    x

--- frontends/steam/install.sh
+++ frontends/steam/install.sh
@@ -8,19 +8,11 @@ CURRENT_ABSOLUTE_DIR=$(
 source $CURRENT_ABSOLUTE_DIR/../../vars.env
 
 if [[ "$SYSTEM" == "steamdeck" ]]; then
-  #controllers icons
+  #controllers
   mkdir -p $STEAM_CONTROLLER_TEMPLATES_DIR
   cp -r "$CURRENT_ABSOLUTE_DIR/configs/controller_base/." $STEAM_CONTROLLER_TEMPLATES_DIR
 
   #controllers icons
   mkdir -p $STEAM_TENFOOT_BINDING_ICONS_DIR
   cp -r "$CURRENT_ABSOLUTE_DIR/files/binding_icons/." $STEAM_TENFOOT_BINDING_ICONS_DIR
-
-  #shortcuts icons
-  mkdir -p "$STEAM_CONFIG_DIR/grid"
-  cp -r "$CURRENT_ABSOLUTE_DIR/files/grid/." "$STEAM_CONFIG_DIR/grid"
-
-  #shortcuts
-  #TODO merge
-  #cp "$CURRENT_ABSOLUTE_DIR/configs/shortcuts.vdf" "$STEAM_CONFIG_DIR"
 fi
